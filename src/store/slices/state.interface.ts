9c75e3e4f33193aacb7efd5ddbf6219d2987ba80
crypt3311 <cryptoninja101@gmail.com>
2022-03-14 14:10:54 +0530

    Changes for pending txns store

--- src/store/slices/state.interface.ts
+++ src/store/slices/state.interface.ts
@@ -1,6 +1,7 @@
 import {IProjectDetailsSlice} from "./project_slice";
+import {IPendingTxn} from "./pending_txn_slice";
 
 export interface IReduxState {
-    // pendingTransactions: IPendingTxn[];
+    pendingTransactions: IPendingTxn[];
     project: IProjectDetailsSlice;
 }
--- src/store/store.ts
+++ src/store/store.ts
@@ -1,10 +1,12 @@
 import { configureStore } from "@reduxjs/toolkit";
 
 import projectReducer from "./slices/project_slice";
+import pendingTransactionsReducer from "./slices/pending_txn_slice";
 
 const store = configureStore({
     reducer: {
         project: projectReducer,
+        pendingTransactions: pendingTransactionsReducer,
     },
     middleware: getDefaultMiddleware => getDefaultMiddleware({ serializableCheck: false }),
 });
