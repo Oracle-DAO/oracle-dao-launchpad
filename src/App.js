74a857254c18a1921bdf7a812dd3ee60a7c725a1
gt28ch <gt28ch@gmail.com>
2022-03-23 11:39:37 +0530

    removed fetch project details from app.js

--- src/App.js
+++ src/App.js
@@ -1,12 +1,11 @@
 import routes from "./Routes";
 
 import { useRoutes } from "react-router-dom";
-import {useAddress, useWeb3Context} from "./hooks";
-import {useEffect, useState} from "react";
+import { useWeb3Context } from "./hooks";
+import { useEffect, useState } from "react";
 
 function App() {
-  const { connect, provider, hasCachedProvider, chainID, connected } = useWeb3Context();
-  const address = useAddress();
+  const { connect, hasCachedProvider } = useWeb3Context();
 
   const [walletChecked, setWalletChecked] = useState(false);
 
