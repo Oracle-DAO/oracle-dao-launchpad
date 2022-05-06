9c1feb005ecc8fa2927cb87cd7cfc28184bb0234
gt28ch <gt28ch@gmail.com>
2022-05-06 22:43:27 +0530

    design changes

--- src/components/home/home.tsx
+++ src/components/home/home.tsx
@@ -1,9 +1,11 @@
 import './home.scss'
-import {Box, Link} from "@mui/material";
+import { Box, Link } from "@mui/material";
 import rocket from "../../assets/img/rocket.webp";
-import * as React from "react";
+import advisory from '../../assets/img/advisory.png';
+import audit from '../../assets/img/audit.png';
+import fundraising from '../../assets/img/fundraising.png';
 
-export function Home(){
+export function Home() {
     return (
         <div className="container pt-5">
             <div className="headline-root">
@@ -23,16 +25,16 @@ export function Home(){
                         </div>
                     </section>
                     <div className="project-button-div mt-3 mb-3">
-                        <Link className="project-button" href = "/projects" underline="none">
+                        <Link className="project-button" href="/projects" underline="none">
                             Projects
                         </Link>
                     </div>
                 </Box>
                 <Box className="headline-right-box">
                     <img
-                        src = {rocket}
-                        alt = "oracle-finance"
-                        className = "headline-img"
+                        src={rocket}
+                        alt="oracle-finance"
+                        className="headline-img"
                     />
                 </Box>
             </div>
@@ -43,18 +45,20 @@ export function Home(){
             </section>
 
             <section id="projects" className="mt-5 container-with-image d-flex p-3 align-items-center">
-                <div className="section-text p-5">
+                <div className="section-text p-4">
+                    <p>Dedicated selectivity</p>
                     <p>Project Selection Criteria</p>
                     <p>Project launching IDO on Oracle LaunchPad needs to meet specific criteria in terms of
                         idea, potential, project development and overall security.
                     </p>
                 </div>
-                <div className="section-image d-none d-sm-block"></div>
+                <div className="section-image criteria d-none d-sm-block"></div>
             </section>
 
             <section className="mt-5 container-with-image d-flex p-3 align-items-center">
-                <div className="section-image d-none d-sm-block"></div>
-                <div className="section-text p-5">
+                <div className="section-image report d-none d-sm-block"></div>
+                <div className="section-text p-4">
+                    <p>Complete enthusiasm</p>
                     <p>Detailed Report Generation</p>
                     <p>Our teams of experts will evaluate each and every project and prepare a detailed report to
                         help investors make an informed decision.
@@ -63,38 +67,44 @@ export function Home(){
             </section>
 
             <section className="mt-5 container-with-image d-flex p-3 align-items-center">
-                <div className="section-text p-5">
+                <div className="section-text p-4">
+                    <p>New structure</p>
                     <p>Tierless System</p>
                     <p>Unlike other launchpads, Oracle LaunchPad does not have a tier system.
                         Investors who have staked ORFI tokens can take part in IDOs</p>
                 </div>
-                <div className="section-image d-none d-sm-block"></div>
+                <div className="section-image tierless-system d-none d-sm-block"></div>
             </section>
 
-            {/*<section className="project-selection-text-container d-flex justify-content-center">*/}
-            {/*    <p>FOR PROJECTS</p>*/}
-            {/*</section>*/}
+            <section className="mt-5 our-help-container">
+                <p>Our Help</p>
+                <p>Supporting Features</p>
+                <div className="p-4 right d-flex align-items-center">
+                    <img src={fundraising} className="d-none d-sm-block" />
+                    <div>
+                        <p>Fundraising</p>
+                        <p>As a fundraising platform, we can assist projects in raising fund effectively through our distribution mechanism, ensuring benefits for
+                            both projects and investors.</p>
+                    </div>
+                </div>
+                <div className="p-4 left d-flex align-items-center">
+                    <img src={advisory} className="d-none d-sm-block" />
+                    <div>
+                        <p>Advisory</p>
+                        <p>We assist projects in developing a cohesive growth and marketing strategy, as well as advising projects on the best course of action for
+                            fund-raising and launch.</p>
+                    </div>
+                </div>
+                <div className="p-4 right d-flex align-items-center">
+                    <img src={audit} className="d-none d-sm-block" />
+                    <div>
+                        <p>Audit</p>
+                        <p>Apart from promoting your projects, we can help you connect to our Audit partners to review, advice and secure your
+                            project's Smart Contract.</p>
+                    </div>
+                </div>
+            </section>
 
-            {/*<div className="mt-5 cards-container">*/}
-            {/*    <div className="p-5 mt-3 flex-grow-1 me-3">*/}
-            {/*        <div/>*/}
-            {/*        <p>Private Sale</p>*/}
-            {/*        <p>Primary ways for users to build wealth. Users can purchase ORCL at discounted price*/}
-            {/*            via bonding and/or can stake ORCL to receive passive yields</p>*/}
-            {/*    </div>*/}
-            {/*    <div className="p-5 mt-3 flex-grow-1 me-3">*/}
-            {/*        <div/>*/}
-            {/*        <p>Treasury</p>*/}
-            {/*        <p>Treasury is the engine of Oracle Finance and backs overall floor price, which ensures underlying*/}
-            {/*            price is independent of market volatility</p>*/}
-            {/*    </div>*/}
-            {/*    <div className="p-5 mt-3 flex-grow-1">*/}
-            {/*        <div />*/}
-            {/*        <p>Rewards Distribution</p>*/}
-            {/*        <p>The total profit generated by the protocol is distributed to all stakers in stable coins,*/}
-            {/*            thus generating a passive yield with relatively low risks</p>*/}
-            {/*    </div>*/}
-            {/*</div>*/}
         </div>
-        )
+    )
 }
